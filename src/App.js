14aa6df376df6e881bda75bbaa9c32fbad7e2908
shubhxms <03.shubhamshah@gmail.com>
2022-05-18 20:20:25 +0530

    make changes to how data is collected and stored

--- src/App.js
+++ src/App.js
@@ -116,6 +116,10 @@ export default function App() {
         dataArr = await InfPortalContract.getData();
         console.log(dataArr);
         setAddArr(dataArr[0]);
+        let tempInfArr;
+        for(let i = 0; i< dataArr[1].length; i++){
+          tempInfArr.push(dataArr[1][i]["value"]);
+        }
         setInfArr(dataArr[1]);
 
       }else{
